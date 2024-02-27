3837e3726bf02fd82cb8a797f8fa579be3964d6e
meblo98 <lomouhamedelbachir@gmail.com>
2024-02-27 13:54:11 +0000

    fin des pages de connexion et d'inscription

---
+++ auth/register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+<?php
+// Informations d'identification
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'boite_idee');
+ 
+// Connexion à la base de données MySQL 
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Vérifier la connexion
+if($conn === false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
+  // récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
+  $username = stripslashes($_REQUEST['username']);
+  $username = mysqli_real_escape_string($conn, $username); 
+  // récupérer l'email et supprimer les antislashes ajoutés par le formulaire
+  $email = stripslashes($_REQUEST['email']);
+  $email = mysqli_real_escape_string($conn, $email);
+  // récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
+  $password = stripslashes($_REQUEST['password']);
+  $password = mysqli_real_escape_string($conn, $password);
+  //requéte SQL + mot de passe crypté
+    $query = "INSERT into `users` (username, email, password)
+              VALUES ('$username', '$email', '".hash('sha256', $password)."')";
+  // Exécuter la requête sur la base de données
+    $res = mysqli_query($conn, $query);
+    if($res){
+       echo "<div class='sucess'>
+             <h3>Vous êtes inscrit avec succès.</h3>
+             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
+       </div>";
+    }
+}else{
+?>
+<div class="container">
+<form class="box" action="" method="post">
+<h1 class="box-logo box-title">Inscription</h1>
+    <h1 class="box-title">S'inscrire</h1>
+  <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required /><br/>
+    <input type="text" class="box-input" name="email" placeholder="Email" required /><br/>
+    <input type="password" class="box-input" name="password" placeholder="Mot de passe" required /><br/>
+    <input type="submit" name="submit" value="S'inscrire" class="box-button" />
+    <p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
+</form>
+</div>
+<?php } ?>
+</body>
+</html>
